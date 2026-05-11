256e4e8473d5d273e69d107222c3f461024890ab
Alexis Cepeda <alexkiller0408@hotmail.com>
2026-05-10 21:52:15 -0500

    feat: crud de tenant terminado

--- PyroCloud.Modules.Identity/Interfaces/ITenantAppService.cs
+++ PyroCloud.Modules.Identity/Interfaces/ITenantAppService.cs
@@ -1,6 +1,22 @@
-﻿namespace PyroCloud.Modules.Identity.Interfaces
+﻿using PyroCloud.Core.Application.Dtos.Identity.Roles;
+using PyroCloud.Core.Application.Dtos.Identity.Tenant;
+
+namespace PyroCloud.Modules.Identity.Interfaces
 {
     public interface ITenantAppService
     {
+
+        Task<IEnumerable<TenantDto>> GetAvaliableTenants();
+
+        Task<TenantResponseDto> GetTenantByIdAsync(Guid id);
+
+        Task<TenantDto> CreateTenant(TenantDto tenant);
+
+        //Task<TenantDto> UpdateRole(TenantDto tenant);
+        Task<TenantDto> UpdateTenant(TenantDto tenant);
+
+        ////Task<TenantDto> DeleteRole(int id);
+        //ToggleTenantActive
+        Task<UpdateTenantStatusDto> ToggleTenantActive(Guid id);
     }
 }
